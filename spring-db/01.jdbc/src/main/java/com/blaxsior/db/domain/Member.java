e1909ae459b93c731778bcf6bd3b75bda2507d60
blaxsior <heejun2798@gmail.com>
2024-04-22 13:03:02 +0900

    study: jdbc 사용법 공부
    jdbc: 자바 진영에서 db 연결, 쿼리, 반환 데이터 처리 방식 등을 표준화화한 인터페이스. 각 DB 벤더는 jdbc를 구현한 구현체를 제공.
    배경: 과거 각 dbms는 서로 다른 연결 방식과 쿼리 데이터 처리 방식을 가졌음. dbms에 직접 의존하기 때문에 개발자는 dbms 바꾸려면 기존 구현 전부 변경 + 새로운 dbms 처리 기능을 새로 배웠어야 함
    해결책: 각 dbms의 Connections, Statement, ResultSet 처리 방식을 표준화. 개발자는 표준을 이용하여 개발하므로, 더 이상 벤더에 의존적이지 않음.
    1. 각 벤더에 맞는 처리 방법을 일일이 배워 처리하지 않아도 됨.
    2. 다른 dbms로의 마이그레이션이 자유로움.
    jdbc가 드라이버를 찾는 방식: 리스트 + 순회
    1. 라이브러리에 등록된 드라이버를 모두 찾아 하나의 리스트로 관리
    2. getConnection 호출 시 db 정보(url, username, password)를 리스트 내 드라이버에 대해 일일이 대입. 커넥션을 반환하면 타겟 dbms 드라이버

---
+++ spring-db/01.jdbc/src/main/java/com/blaxsior/db/domain/Member.java
@@ -0,0 +1,23 @@
+package com.blaxsior.db.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Member {
+    private String memberId;
+    private int money;
+
+    public Member() {
+    }
+
+    public Member(String memberId, int money) {
+        this.memberId = memberId;
+        this.money = money;
+    }
+}
---
+++ spring-db/01.jdbc/src/test/java/com/blaxsior/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaxsior.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
